17053fd16332c756dedca5b15231d655450afd6d
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-17 12:03:14 -0400

    Added const variables to require libraries I am about to download via npm

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const mustache = require('mustache-express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const app = express();
