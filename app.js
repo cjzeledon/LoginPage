f03131422b8d86b92d0c93fb159200d9357bf795
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-17 13:33:12 -0400

    Added a users object with arrays of usernames/passwords. Added comments for other block of codes.

--- app.js
+++ app.js
@@ -11,14 +11,24 @@ app.set('view engine', 'mustache');
 // app.use(express.static('public'));
 // Decide if you need to use the static page and how it really works and what its purpose really is
 
+// {} are arrays and [] are objects
+const users = [
+  {username: 'ayanna', password='minecraft', logins: 0},
+  {username: 'melania', password='talks', logins: 0},
+  {username: 'sophia', password='runs', logins: 0},
+];
+
+// This is to create a login web page at the root path.
 app.get('/', function (request, respond){
   respond.render('login');
 });
 
+// This creates a welcome page after the login is successful.
 app.get('/welcome', function(request, respond){
   respond.render('welcome');
 });
 
+// This creates a port 3000 so that the app can "listen" in to. Basically grants access?
 app.listen(3000, function(){
   console.log('Node.js Rule!');
 })
